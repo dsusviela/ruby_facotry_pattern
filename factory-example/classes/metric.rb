00bdefc08c48f59dc4df454be7ebbc9cce62bcd9
Daniel Susviela <d.susviela@gmail.com>
2021-11-23 19:33:11 +0100

    Clear the metric factory file

--- factory-example/classes/metric.rb
+++ factory-example/classes/metric.rb
@@ -1,26 +1,24 @@
-# can be commented out in favor of the factory
-
 require_relative "./downloads"
 require_relative "./power"
 require_relative "./revenues"
 require_relative "./reviews"
 
 class Metric
-  # def self.put_information(metric)
-  #   klass = if metric == "downloads"
-  #     Downloads
-  #   elsif metric == "power"
-  #     Power
-  #   elsif metric == "revenues"
-  #     Revenues
-  #   elsif metric == "reviews"
-  #     Reviews
-  #   else
-  #     raise RuntimeError
-  #   end
+  def self.put_information(metric)
+    klass = if metric == "downloads"
+      Downloads
+    elsif metric == "power"
+      Power
+    elsif metric == "revenues"
+      Revenues
+    elsif metric == "reviews"
+      Reviews
+    else
+      raise RuntimeError
+    end
 
-  #   klass.new.put_information
-  # end
+    klass.new.put_information
+  end
 
   def initialize
     raise NotImplementedError
